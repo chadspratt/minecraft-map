38aba5e7b744aa11d2cb74bd654c042184d8082c
Chad Spratt <chad@dogtato.net>
2013-11-19 04:00:22 -0300

    progress towards getting it to scale and position the initial map

--- js/getmaps.js
+++ js/getmaps.js
@@ -6,23 +6,22 @@ var httpRequest,
     last = {x: 0, y: 0},
     viewCenter = {x: 0, y: 0},
     isDown = false,
-    scale = 1;
+    scale = 1,
+    mapSize = 128 * 8;
 
 function drawMap() {
     'use strict';
     var x,
         y,
-        height = 128 * 8,
-        width = 128 * 8,
         img,
         map,
         mapName;
     // resize the canvas to fit the window
-    // ctx.canvas.width = window.innerWidth;
-    // ctx.canvas.height = window.innerHeight;
-    // draw white background
+    // canvas.width = window.innerWidth;
+    // canvas.height = window.innerHeight;
+    // clear canvas with a white background
     ctx.save();
-    ctx.setTransform(scale, 0, 0, scale, 0, 0);
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.fillStyle = "#FFFFFF";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.restore();
@@ -36,7 +35,7 @@ function drawMap() {
             img = document.createElement("img");
             img.src = map['Image location'];
             // window.alert(img.src);
-            ctx.drawImage(img, x, y, width, height);
+            ctx.drawImage(img, x, y, mapSize, mapSize);
         }
     }
 }
@@ -57,7 +56,7 @@ canvas.onmousedown = function (e) {
                    y: e.pageY - this.offsetTop - last.y};
 };
 
-canvas.onmouseup = function (e) {
+document.body.onmouseup = function (e) {
     'use strict';
     isDown = false;
     last = {x: e.pageX - this.offsetLeft - startCoords.x,
@@ -101,9 +100,19 @@ function mapDataReceived() {
                     }
                 }
             }
+            // coordinates were based on map center, adjust for edge
+            maxX += mapSize / 2;
+            maxY += mapSize / 2;
+            minX -= mapSize / 2;
+            minY -= mapSize / 2;
             viewCenter.x = (minX + maxX) / 2;
             viewCenter.y = (minY + maxY) / 2;
-            ctx.setTransform(scale, 0, viewCenter.x, scale, 0, viewCenter.y);
+            last = {x: canvas.width / 2 - viewCenter.x,
+                    y: canvas.height / 2 - viewCenter.y};
+            // scale so the whole map fits
+            scale = Math.max(canvas.width, canvas.height) /
+                    Math.max(maxX - minX, maxY - minY);
+            ctx.setTransform(scale, 0, 0, scale, last.x, last.y);
             setInterval(drawMap, 100); // set the animation into motion
         }
     }
