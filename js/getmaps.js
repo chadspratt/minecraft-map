a27b918334c3d217957b027037495c2e0555a4a0
Chad Spratt <chad@dogtato.net>
2013-11-23 03:10:34 -0300

    Closes #10, draw order to cover "map_x" text

--- js/getmaps.js
+++ js/getmaps.js
@@ -1,5 +1,6 @@
 var httpRequest,
     mapData,
+    mapArray = [],
     canvas = document.getElementById("mapCanvas"),
     ctx = canvas.getContext("2d"),
     startCoords = {x: 0, y: 0},
@@ -16,10 +17,8 @@ function drawMap() {
         y,
         img,
         map,
-        mapName;
-    // resize the canvas to fit the window
-    // canvas.width = window.innerWidth;
-    // canvas.height = window.innerHeight;
+        // mapName,
+        i;
     // clear canvas with a white background
     ctx.save();
     ctx.setTransform(1, 0, 0, 1, 0, 0);
@@ -28,18 +27,15 @@ function drawMap() {
     // reapply scale and translation
     ctx.restore();
     // draw maps
-    for (mapName in mapData.results) {
-        if (mapData.results.hasOwnProperty(mapName)) {
-            map = mapData.results[mapName].printouts;
-            // coords are map centers, need top left corners
-            x = map['X coord'] - mapAdjust;
-            // n/s is measured as z in minecraft
-            y = map['Z coord'] - mapAdjust;
-            img = document.createElement("img");
-            img.src = map['Image location'];
-            // window.alert(img.src);
-            ctx.drawImage(img, x, y, mapSize, mapSize);
-        }
+    for (i = 0; i < mapArray.length; i += 1) {
+        map = mapArray[i];
+        x = map['X coord'];
+        // n/s is measured as z in minecraft
+        y = map['Z coord'];
+        img = document.createElement("img");
+        img.src = map['Image location'];
+        // coords are map centers, need top left corners
+        ctx.drawImage(img, x - mapAdjust, y - mapAdjust, mapSize, mapSize);
     }
 }
 
@@ -65,6 +61,28 @@ canvas.onmousedown = function (e) {
                    y: e.pageY - canvas.offsetTop - last.y};
 };
 
+function sortmaps(a, b) {
+    'use strict';
+    var x0 = a['X coord'],
+        x1 = b['X coord'],
+        y0 = a['Z coord'],
+        y1 = b['Z coord'],
+        x0Diff = x0 - viewCenter.x,
+        y0Diff = y0 - viewCenter.y,
+        x1Diff = x1 - viewCenter.x,
+        y1Diff = y1 - viewCenter.y,
+        dist0 = Math.sqrt(x0Diff * x0Diff + y0Diff * y0Diff),
+        dist1 = Math.sqrt(x1Diff * x1Diff + y1Diff * y1Diff);
+    // put things below a x = -y diagonal first
+    if (x0Diff > -y0Diff) {
+        dist0 = -dist0;
+    }
+    if (x1Diff > -y1Diff) {
+        dist1 = -dist1;
+    }
+    return dist0 - dist1;
+}
+
 document.body.onmouseup = function (e) {
     'use strict';
     // check that the click started on the canvas
@@ -72,6 +90,10 @@ document.body.onmouseup = function (e) {
         isDown = false;
         last = {x: e.pageX - canvas.offsetLeft - startCoords.x,
                 y: e.pageY - canvas.offsetTop - startCoords.y};
+        viewCenter = {x: (canvas.width / 2 - last.x) / scale,
+                      y: (canvas.height / 2 - last.y) / scale};
+        // sort maps by their distance from the view center
+        mapArray.sort(sortmaps);
     }
 };
 
@@ -87,15 +109,13 @@ function mapDataReceived() {
         map;
     if (httpRequest.readyState === 4) {
         if (httpRequest.status === 200) {
-            // testing
-            // var testarea = document.getElementById('debugarea');
-            // testarea.innerHTML = httpRequest.responseText;
             // process json object
             mapData = JSON.parse(httpRequest.responseText);
             // determine the center of 
             for (mapName in mapData.results) {
                 if (mapData.results.hasOwnProperty(mapName)) {
                     map = mapData.results[mapName].printouts;
+                    mapArray.push(map);
                     curX = map['X coord'][0];
                     curY = map['Z coord'][0];
                     if (curX > maxX) {
@@ -124,10 +144,9 @@ function mapDataReceived() {
                     Math.max(maxX - minX, maxY - minY);
             last = {x: canvas.width / 2 - viewCenter.x * scale,
                     y: canvas.height / 2 - viewCenter.y * scale};
-            ctx.setTransform(1, 0, 0, 1, 0, 0);
-            ctx.translate(last.x, last.y);
-            ctx.scale(scale, scale);
-            // ctx.setTransform(scale, 0, 0, scale, last.x, last.y);
+            ctx.setTransform(scale, 0, 0, scale, last.x, last.y);
+            // sort maps by their distance from the view center
+            mapArray.sort(sortmaps);
             setInterval(drawMap, 100); // set the animation into motion
         }
     }
