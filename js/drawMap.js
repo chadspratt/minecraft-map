7b008a420939bf2e802002b43344ece57920a79f
Chad Spratt <chad@dogtato.net>
2013-12-28 19:55:30 -0300

    Closes #28 non-square feature markers

--- js/drawMap.js
+++ js/drawMap.js
@@ -1,7 +1,7 @@
 /*global $ */
 var mainApp;
 
-function Boundary(centerX, centerY, width, height, image) {
+function Boundary(centerX, centerY, width, height) {
     'use strict';
     this.centerX = centerX;
     this.centerY = centerY;
@@ -11,17 +11,6 @@ function Boundary(centerX, centerY, width, height, image) {
     this.right = this.left + width;
     this.top = centerY - height / 2;
     this.bottom = this.top + height;
-    if (image !== null) {
-        this.image = document.createElement('img');
-        this.image.src = image;
-        // maybe move this up to processMapData
-        this.image.onload = function imageLoaded() {
-            // not the best solution
-            mainApp.mapCanvas.needUpdate = true;
-        };
-    } else {
-        this.image = null;
-    }
     this.contains = function (boundary) {
         return (boundary.left < this.right &&
                 boundary.right > this.left &&
@@ -48,6 +37,31 @@ function Boundary(centerX, centerY, width, height, image) {
     };
 }
 
+function MapImagery(centerX, centerY, imageURL) {
+    'use strict';
+    var mapSize = 128 * 8;
+    this.boundary = new Boundary(centerX, centerY, mapSize, mapSize);
+    this.image = document.createElement('img');
+    this.image.onload = function () {
+        mainApp.mapCanvas.needUpdate = true;
+    };
+    this.image.src = imageURL;
+}
+
+function Feature(x, y, imageURL) {
+    'use strict';
+    var self = this;
+    this.image = document.createElement('img');
+    this.image.onload = function () {
+        self.boundary = new Boundary(x, y, this.width, this.height);
+        mainApp.mapCanvas.needUpdate = true;
+    };
+    this.image.src = imageURL;
+    this.distanceFrom = function (x1, y1) {
+        return this.boundary.squareDistanceFromCenter(x1, y1);
+    };
+}
+
 function Category(categoryName) {
     'use strict';
     this.name = categoryName;
@@ -68,7 +82,6 @@ function MapData() {
     var self = this;
     this.maps = [];
     this.categories = {};
-    this.mapSize = 128 * 8;
     this.boundary = null;
 
     // orders maps from SE to NW, so that NW will be drawn on top of SE
@@ -76,13 +89,13 @@ function MapData() {
     this.sortMaps = function (a, b) {
         var comparator = 0;
         // compare based on the coordinates with a bigger difference
-        if (Math.abs(a.left - b.left) >
-                Math.abs(a.top - b.top)) {
+        if (Math.abs(a.boundary.left - b.boundary.left) >
+                Math.abs(a.boundary.top - b.boundary.top)) {
             // sort descending (east to west)
-            comparator =  b.left - a.left;
+            comparator =  b.boundary.left - a.boundary.left;
         } else {
             // sort descending (south to north)
-            comparator = b.top - a.top;
+            comparator = b.boundary.top - a.boundary.top;
         }
         return comparator;
     };
@@ -91,29 +104,28 @@ function MapData() {
             maxY = -Infinity,
             minX = Infinity,
             minY = Infinity,
-            mapBoundary,
+            mapImage,
             centerX,
             centerY;
         $.each(mapData, function (mapName, map) {
             if (mapData.hasOwnProperty(mapName)) {
-                // create and store in an array for ordered drawing later
-                mapBoundary = new Boundary(map['X coord'],
-                                           map['Z coord'],
-                                           self.mapSize, self.mapSize,
-                                           map['Image location']);
-                self.maps.push(mapBoundary);
+                mapImage = new MapImagery(map['X coord'],
+                                          map['Z coord'],
+                                          map['Image location']);
+                // store in an array for ordered drawing later
+                self.maps.push(mapImage);
                 // determine the center of all maps
-                if (mapBoundary.right > maxX) {
-                    maxX = mapBoundary.right;
+                if (mapImage.boundary.right > maxX) {
+                    maxX = mapImage.boundary.right;
                 }
-                if (mapBoundary.left < minX) {
-                    minX = mapBoundary.left;
+                if (mapImage.boundary.left < minX) {
+                    minX = mapImage.boundary.left;
                 }
-                if (mapBoundary.bottom > maxY) {
-                    maxY = mapBoundary.bottom;
+                if (mapImage.boundary.bottom > maxY) {
+                    maxY = mapImage.boundary.bottom;
                 }
-                if (mapBoundary.top < minY) {
-                    minY = mapBoundary.top;
+                if (mapImage.boundary.top < minY) {
+                    minY = mapImage.boundary.top;
                 }
             }
         });
@@ -121,24 +133,22 @@ function MapData() {
         // map coordinates of the center of the visible map
         centerX = (minX + maxX) / 2;
         centerY = (minY + maxY) / 2;
-        // the bounds of all map images
+        // the bounds of all map images. arbitrary default view
         this.boundary = new Boundary(centerX, centerY,
-                                            (maxX - minX),
-                                            (maxY - minY),
-                                            null);
+                                     maxX - minX, maxY - minY);
     };
     // doesn't reference 'this'
-    this.processFeatures = function (features, categoryIcon) {
+    this.processFeatures = function (featureData, categoryIcon) {
         var featureName,
             feature,
             featureBoundary,
             // return value
-            featureBoundaries = {},
+            features = {},
             x,
             y;
-        for (featureName in features) {
-            if (features.hasOwnProperty(featureName)) {
-                feature = features[featureName];
+        for (featureName in featureData) {
+            if (featureData.hasOwnProperty(featureName)) {
+                feature = featureData[featureName];
                 x = feature['X coord'];
                 // minecraft stores N/S as z
                 y = feature['Z coord'];
@@ -147,13 +157,12 @@ function MapData() {
                     if (feature.Icon === null) {
                         feature.Icon = categoryIcon;
                     }
-                    // store as point (0 height and width)
-                    featureBoundary = new Boundary(x, y, 0, 0, feature.Icon);
-                    featureBoundaries[featureName] = featureBoundary;
+                    features[featureName] = new Feature(x, y,
+                                                        feature.Icon);
                 }
             }
         }
-        return featureBoundaries;
+        return features;
     };
     this.processCategory = function (categoryName, categoryData, parentIcon) {
         var category = new Category(categoryName);
@@ -218,7 +227,7 @@ function MapCanvas() {
     this.needUpdate = false;
     this.loadMap = function () {
         this.mapData.load();
-        $(this.mapData).on('dataLoaded', function () {
+        $(this.mapData).on('dataLoaded', function mapDataLoaded() {
             // set initial transformation
             self.viewCenter = {x: self.mapData.boundary.centerX,
                                y: self.mapData.boundary.centerY};
@@ -249,24 +258,28 @@ function MapCanvas() {
         this.canvasContext.restore();
     };
     this._drawMaps = function () {
-        var mapBoundary,
+        var mapImagery,
             i;
         for (i = 0; i < this.mapData.maps.length; i += 1) {
-            mapBoundary = this.mapData.maps[i];
+            mapImagery = this.mapData.maps[i];
             // check that map will be visible
-            if (this.boundary.contains(mapBoundary)) {
-                this.canvasContext.drawImage(mapBoundary.image,
-                                        mapBoundary.left, mapBoundary.top,
-                                        mapBoundary.width, mapBoundary.height);
+            if (this.boundary.contains(mapImagery.boundary)) {
+                this.canvasContext.drawImage(mapImagery.image,
+                                             mapImagery.boundary.left,
+                                             mapImagery.boundary.top,
+                                             mapImagery.boundary.width,
+                                             mapImagery.boundary.height);
             }
         }
     };
     this._drawFeatures = function () {
         var // divide by scale to keep the icon size constant
-            featureSize = self.featureIconSize / this.scale,
-            edgeAdjust = featureSize / 2,
+            featureSize = this.featureIconSize / this.scale,
+            scaleFactor,
             left,
-            top;
+            top,
+            width,
+            height;
         // store all features that get drawn
         this.visibleFeatures = {};
         // go through each category that's turned on
@@ -275,12 +288,22 @@ function MapCanvas() {
                     category.visible) {
                 $.each(category.features, function (featureName, feature) {
                     if (category.features.hasOwnProperty(featureName) &&
-                            self.boundary.containsCenterOf(feature)) {
-                        left = feature.centerX - edgeAdjust;
-                        top = feature.centerY - edgeAdjust;
+                            self.boundary.containsCenterOf(feature.boundary)) {
+                        // scale so the largest dimension is featureSize
+                        if (feature.boundary.width > feature.boundary.height){
+                            scaleFactor = featureSize / feature.boundary.width;
+                            width = featureSize;
+                            height = feature.boundary.height * scaleFactor;
+                        } else {
+                            scaleFactor = featureSize / feature.boundary.height;
+                            width = feature.boundary.width * scaleFactor;
+                            height = featureSize;
+                        }
+                        left = feature.boundary.centerX - width / 2;
+                        top = feature.boundary.centerY - height / 2;
                         // draw the feature
                         self.canvasContext.drawImage(feature.image, left, top,
-                                                     featureSize, featureSize);
+                                                     width, height);
                         // and store it for checking mouseover
                         self.visibleFeatures[featureName] = feature;
                     }
@@ -312,7 +335,7 @@ function MapCanvas() {
                 this.boundary.containsPoint(x, y)) {
             $.each(this.visibleFeatures, function (featureName, feature) {
                 if (self.visibleFeatures.hasOwnProperty(featureName)) {
-                    featureDistance = feature.squareDistanceFromCenter(x, y);
+                    featureDistance = feature.distanceFrom(x, y);
                     if (featureDistance < nearestDistance) {
                         nearestDistance = featureDistance;
                         nearbyFeature = featureName;
@@ -495,7 +518,7 @@ function MainApp() {
     };
     this.init = function () {
         this.mapCanvas.loadMap();
-        $(this.mapCanvas).on('canvasReady', function () {
+        $(this.mapCanvas).on('canvasReady', function mapCanvasReady() {
             $('#categories').html(self.createCheckboxes('Features'));
             self.setCheckboxHandlers();
             setInterval(self.mapCanvas.draw, 100); // start drawing
